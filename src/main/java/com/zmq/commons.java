1107b28fb8e866fd881702510a4b7375be8e467b
zhangmaoquan <hegongdajava@163.com>
2018-10-30 17:05:25 +0800

    Revert "第八次"
    This reverts commit 05f249c642da10b3d87ef0d137af4ce1b3c44bb2.

--- src/main/java/com/zmq/commons.java
+++ src/main/java/com/zmq/commons.java
@@ -2,6 +2,6 @@ package com.zmq;
 
 public class commons {
     public static void main(String[] args) {
-        System.out.println("这是我可是你好极了可是我们是的个bug");
+        System.out.println("这是我可是你好极了的个bug");
     }
 }
