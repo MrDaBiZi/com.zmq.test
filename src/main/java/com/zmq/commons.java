460e5020319212b884be7c14254a4830ff7b5ece
zhangmaoquan <hegongdajava@163.com>
2018-10-31 10:41:29 +0800

    1031

--- src/main/java/com/zmq/commons.java
+++ src/main/java/com/zmq/commons.java
@@ -2,6 +2,17 @@ package com.zmq;
 
 public class commons {
     public static void main(String[] args) {
+        try {
+            test();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         System.out.println("这是我可是第三方审计的律师费你好文件夹了极了的个bug");
+    }
+
+    private static void test() {
+        if (true) {
+            throw new NullPointerException();
+        }
     }
 }
