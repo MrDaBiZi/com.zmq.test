e9739d80b7a01deb7b515d67c2abee7a6f7005f4
zhangmaoquan <hegongdajava@163.com>
2018-10-31 12:42:10 +0800

    1000

--- src/main/java/com/zmq/commons.java
+++ src/main/java/com/zmq/commons.java
@@ -4,4 +4,7 @@ public class commons {
     public static void main(String[] args) {
         System.out.println("咳惹味认为热望日发的时热热热热间微软认热热热犯得上发射点房贷首付为热望浪发射点发射点费多少惹味认为认为v咳dierci犯得上范德萨范德萨范德萨反对法士大夫发生的家乐福的是这是fdsfsdf我可fdsfsdlfsd是第fdsfsdfjjdlf三方fdfldsfjsl审计的律fdsfjdslfdsj师费你好文件夹了极了的个bug");
     }
+    private static void test() {
+
+    }
 }
